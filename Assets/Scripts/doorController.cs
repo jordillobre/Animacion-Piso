5afbabb8d9158fe0fcaa2b764d6ac255355ece5c
uji-game <jordillobre@gmail.com>
2021-04-16 13:37:12 +0200

    arreglos

--- Assets/Scripts/doorController.cs
+++ Assets/Scripts/doorController.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class doorController : MonoBehaviour{
 
-    //Spublic Animator animacion;
+    public Animator animacion;
 
     public bool theDoorIs;
 
     // Start is called before the first frame update
     public void Start()
     {
-        /*animacion = this.GetComponent<Animator>();
+        animacion = this.GetComponent<Animator>();
 
         if (Input.GetKey(KeyCode.Space))
         {
             theDoorIs = !theDoorIs;
             animacion.SetBool("theDoorIs", theDoorIs);
-        }*/
+        }
     }
 
 }
